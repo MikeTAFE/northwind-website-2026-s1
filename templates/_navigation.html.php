808a7c9fecfe8900073b9d0e596c732bbef9ffa0
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2026-03-16 21:01:12 +1100

    Added a navigation include with list-driven links

---
+++ templates/_navigation.html.php
@@ -0,0 +1,25 @@
+<?php
+
+  // Navigation items
+  $navLinks = [
+    // "page.php" => "Nav Title",
+    "index.php" => "Home",
+    "about.php" => "About us",
+    "services.php" => "Our services",
+  ];
+
+  // Get the currently-loaded page (PHP script), e.g. index.php
+  $currentPage = basename($_SERVER["SCRIPT_NAME"]);
+
+?>
+<ul>
+  <!-- <li class="active"><a href="index.php">Home</a></li> -->
+  <?php foreach ($navLinks as $linkHref => $linkText): ?>
+  <?php
+    // Check if current page
+    // (condition) ? true : false
+    $cssClass = ($linkHref === $currentPage) ? "active" : "";
+  ?>
+    <li class="<?= $cssClass ?>"><a href="<?= $linkHref ?>"><?= $linkText ?></a></li>
+  <?php endforeach ?>
+</ul>
